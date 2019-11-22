7eccf4590d78770a9adb5f3f45a38f0b72897350
Sharad Chand <sharad@sharadchand.com>
2019-11-22 08:33:44 +0530

    Initial commit

---
+++ 02_variables/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	const num int = 3
+
+	var flt float64 = 3.2
+
+	var truthy bool = true
+
+	var char rune = 'a'
+
+	var str string = "a"
+
+	var arr []string = []string{"a", "b", "c"}
+
+	fmt.Println(num)
+	fmt.Println(flt)
+	fmt.Println(truthy)
+	fmt.Println(char)
+	fmt.Println(str)
+	fmt.Println(arr)
+}
---
+++ 02_variables/main.js
@@ -0,0 +1,15 @@
+const num = 3;
+
+let flt = 3.2;
+
+let a = true;
+
+let c = "a";
+
+let arr = ["a", "b", "c", "d"];
+
+console.log(num);
+console.log(flt);
+console.log(a);
+console.log(c);
+console.log(arr);
