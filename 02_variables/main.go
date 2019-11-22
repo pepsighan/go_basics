05917d08f71d28b61e9cd4192d40efba70757857
Sharad Chand <sharad@sharadchand.com>
2019-11-22 21:50:07 +0530

    Also include zero value variables

--- 02_variables/main.go
+++ 02_variables/main.go
@@ -21,4 +21,26 @@ func main() {
 	fmt.Println(char)
 	fmt.Println(str)
 	fmt.Println(arr)
+
+	zeroValue()
+}
+
+// Unassigned variables have zero values.
+// Most of the primitive types have non-nil zero values and all reference types have nil as its zero value.
+func zeroValue() {
+	var num int
+	var flt float64
+	var falsy bool
+	var char rune
+	var str string
+	var arr []string
+	var ref *string
+
+	fmt.Println(num == 0)
+	fmt.Println(flt == 0)
+	fmt.Println(falsy == false)
+	fmt.Println(char == 0x00)
+	fmt.Println(str == "")
+	fmt.Println(arr == nil)
+	fmt.Println(ref == nil)
 }
